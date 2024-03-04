e45db0779a3550473e297966e783de38cb186aac
JTepjuk <Jaturon.tepjuk@pexa.com.au>
2024-03-04 16:58:50 +1100

    add tasks in gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,3 +19,41 @@ tasks.test {
 kotlin {
     jvmToolchain(17)
 }
+
+tasks.register("fib"){
+    description = "Calculate Fibo"
+
+    val count = properties["count"]?.toString()?.toInt() ?: 10
+
+    doLast {
+        val fibo = generateSequence(1 to 1) { it.second to it.first + it.second }
+            .map { it.first }
+            .take(count)
+            .toList()
+        println(fibo)
+    }
+}
+
+tasks.register<Fib>("fib2"){
+    count.set(
+        properties["count"]?.toString()?.toInt() ?: 10
+    )
+}
+
+abstract class Fib : DefaultTask() {
+    @get:Input
+    abstract val count : Property<Int>
+
+    @TaskAction
+    fun fib() {
+        if(count.get() < 0) {
+            throw IllegalArgumentException("Count must be positive")
+        }
+
+        val fibo = generateSequence(1 to 1) { it.second to it.first + it.second }
+            .map { it.first }
+            .take(count.get())
+            .toList()
+        println(fibo)
+    }
+}
