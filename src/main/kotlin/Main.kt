6dc0c4291df978dd9c73c6968e1ff9a9f219d168
JTepjuk <Jaturon.tepjuk@pexa.com.au>
2024-03-27 16:04:18 +1100

    create a function to group files bymonth

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,27 @@
 package org.example
 
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
 fun main() {
-    println("Hello World!")
+    val directoryPath = "/Users/jtepjuk/Downloads/ScreenShots"
+
+    val directory = File(directoryPath)
+
+    val files = directory.listFiles() ?: emptyArray()
+
+    files.forEach { file ->
+        val modifiedDate = Date(file.lastModified())
+        val month = SimpleDateFormat("MM", Locale.getDefault()).format(modifiedDate)
+
+        val monthDirectory = File("$directory/$month")
+
+        if(!monthDirectory.exists()) {
+            monthDirectory.mkdir()
+        }
+
+        file.copyTo(File(monthDirectory, file.name))
+        file.delete()
+    }
 }
